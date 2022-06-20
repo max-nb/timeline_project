bb6615146d6f1a5b665224bb769f19fa12cf6dac
max-nb <maxi.biscaia54@gmail.com>
2022-06-19 22:12:10 -0300

    feat: implementado o limitador de caracteres do tweet

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -8,36 +8,70 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./home.component.scss']
 })
 
-export class HomeComponent implements OnInit {  
+export class HomeComponent implements OnInit {
 
-  tweetText:string;
+  tweetText: string;
 
-  arrayTweets: Tweet[]=[];
+  arrayTweets: Tweet[] = [];
 
   newTweet: Tweet;
+
+  typeCount = 0;
+
+  progressBarCount = 0;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
-  getAllTweets(){
+  getAllTweets() {
 
   }
 
-  postTweet(){
-    this.arrayTweets.push({id:this.arrayTweets.length+1, comment:this.tweetText, img:'./../../../assets/user.jpg', date:'asdfasdf', name:'Maximiliano', user: '@maxUser'})
+  postTweet() {
+    this.arrayTweets.push({ id: this.arrayTweets.length + 1, comment: this.tweetText, img: './../../../assets/user.jpg', date: 'asdfasdf', name: 'Maximiliano', user: '@maxUser' })
 
     console.log(this.arrayTweets);
 
-    this.tweetText = '';  
+    this.tweetText = '';
   }
-  
 
-  deleteTweet(objTweet){
+
+  deleteTweet(objTweet) {
     this.arrayTweets.forEach((element, index) => {
-      if(element==objTweet) this.arrayTweets.splice(index,1);
-   });
+      if (element == objTweet) this.arrayTweets.splice(index, 1);
+    });
   }
+
+  increaseProgressType() {
+    this.typeCount = this.typeCount + 1;
+
+    this.progressBarCount = this.progressBarCount + (100 / 130);
+    console.log(this.progressBarCount)
+  }
+
+  decreaseProgressType(event) {
+
+    if (event.keyCode == 8) {
+
+      this.typeCount = this.typeCount - 1;
+
+      this.progressBarCount = this.progressBarCount - (100 / 130);
+
+    } else if (event.keyCode != 8 && this.typeCount < 130) {
+      
+      this.typeCount = this.typeCount + 1;
+
+      this.progressBarCount = this.progressBarCount + (100 / 130);
+    }
+
+    if (!this.tweetText) {
+      this.progressBarCount = 0;
+      this.typeCount = 0;
+    }
+
+  }
+
 
 }
