93c2084796da297ebe4099f52904dafb70d217d7
max-nb <maxi.biscaia54@gmail.com>
2022-06-20 21:34:02 -0300

    feat: ajustada a lógica do contador de caracteres

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -16,84 +16,81 @@ export class HomeComponent implements OnInit {
 
   newTweet: Tweet;
 
-  typeCount = 0;
-
   progressBarCount = 0;
+
+  textPasteSize = 0;
 
   constructor(private tweetService : TweetServiceService) { }
 
   ngOnInit(): void {
+
     this.getAllTweets();
+
   }
 
   getAllTweets() {
-    this.arrayTweets = this.tweetService.getTweetList();
+
+    if(this.tweetService.getTweetList()){
+    
+      this.arrayTweets = this.tweetService.getTweetList();
+    
+    }
   }
 
   postTweet() {
-    this.arrayTweets.push({ id: this.arrayTweets.length + 1, comment: this.tweetText, img: './../../../assets/user.jpg', name: 'Maximiliano', user: '@maxUser' })
+
+    this.arrayTweets?.push({ id: this.arrayTweets.length + 1, comment: this.tweetText, img: './../../../assets/user.jpg', name: 'Maximiliano', user: '@maxUser' })
+ 
     this.tweetService.postTweetList(this.arrayTweets);
+
     this.tweetText = '';
+
+    this.progressBarCount = 0;
   }
 
 
   deleteTweet(objTweet) {
     this.arrayTweets.forEach((element, index) => {
+
       if (element == objTweet) this.arrayTweets.splice(index, 1);
+
     });
 
     this.tweetService.postTweetList(this.arrayTweets);
-  }
-
-  increaseProgressType() {
-    this.typeCount = this.typeCount + 1;
-
-    this.progressBarCount = this.progressBarCount + (100 / 130);
-    console.log(this.progressBarCount)
   }
 
   typeProgress(event) {
 
     if (event.keyCode == 8) {
 
-      this.typeCount = this.typeCount - 1;
-
       this.progressBarCount = this.progressBarCount - (100 / 130);
 
-    } else if (event.keyCode != 8 && event.keyCode != 17 && this.typeCount < 130) {
-      
-      this.typeCount = this.typeCount + 1;
+    } else if (event.keyCode != 8 && !event.ctrlKey && this.tweetText?.length < 130) {
 
       this.progressBarCount = this.progressBarCount + (100 / 130);
+      
     }
+  }
 
-    if (!this.tweetText) {
+  onPaste(event: ClipboardEvent) {   
+
+      if(this.tweetText?.length <= 130){
+
+        this.progressBarCount += event.clipboardData.getData('text').length * (100/130);
+
+      }else{
+        event.preventDefault();
+      }
+   
+  }
+
+  clearProgress(){
+    
+    if(this.tweetText?.length==0){
 
       this.progressBarCount = 0;
 
-      this.typeCount = 0;
     }
-
-  }
-
-  onPaste(event: ClipboardEvent) {  
-      
-    let textPaste = event.clipboardData.getData('text');
-
-      if((this.typeCount + textPaste.length) < 130){
-
-        this.typeCount += textPaste.length;
-
-        this.progressBarCount += textPaste.length * (100/130);
-
-      }else{
-        let rest = (130-this.typeCount);
-
-        this.typeCount += rest;
-
-        this.progressBarCount += rest * (100/130)
-      }
-   
   }
 
 }
